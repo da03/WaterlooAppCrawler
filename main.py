2cc67a8fd8576328958c93bbae9218c335c69372
da03 <dengyuntian@gmail.com>
2024-01-18 07:24:39 -0500

    .

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import os
 import time
 from selenium.webdriver.support.ui import Select
